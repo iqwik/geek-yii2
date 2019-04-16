722d778234d042db4f46948a194972a970d6f404
Зубарев Артем <azubarev@caroperator.ru>
2019-04-16 08:59:28 +0300

    4.Сделать собственный валидатор в виде класса | ч.2

--- validators/PhoneValidator.php
+++ validators/PhoneValidator.php
@@ -6,20 +6,23 @@ use yii\validators\Validator;
 
 class PhoneValidator extends Validator {
 
+    public $pattern = '/^\+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/';
+    public $message = 'Телефон должен быть в формате +7-999-99-99';
+
     public function init() {
         parent::init();
     }
 
     public function validateAttribute($model, $attribute) {
 
-        if (!preg_match('/^\+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/', $model->$attribute))
-            $model->addError($attribute, 'Телефон должен быть в формате +7-999-99-99');
+        if (!preg_match($this->pattern, $model->$attribute))
+            $model->addError($attribute, $this->message);
     }
 
     public function clientValidateAttribute($model, $attribute, $view) {
         return <<<JS
-    if (!/^\+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/.test(value)) {
-        messages.push("Телефон должен быть в формате +7-999-99-99");
+    if (!$this->pattern.test(value)) {
+        messages.push("$this->message");
     } 
 JS;
     }
